01ce99ecd38fcf22f8a1c19ef0aee82348873f0c
Jmalone20 <jasmine.malone@patelhs.org>
2020-09-24 17:32:52 +0000

    I put a space between the quotation marks and the word years

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
    int myAge = 14; 
 
-   System.out.println(" I am " + myAge + "years old.");
+   System.out.println(" I am " + myAge + " years old.");
 
    //If Else Statements
 
